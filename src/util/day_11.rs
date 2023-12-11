62e165a308ee760a4eb2e3c1f248a51030216d4c
Raymond Kalander <raymond.kalander@getswish.se>
2023-12-11 17:56:02 +0100

    refactor day 11

--- src/util/day_11.rs
+++ src/util/day_11.rs
@@ -10,73 +10,12 @@ pub fn solve(input : String, part: Part) -> String {
 }
 
 fn parse(lines : Vec<&str>) -> Vec<Vec<char>> {
-    let mut galaxy = lines.iter()
+    let galaxy = lines.iter()
         .map(|line| line.chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
-
-    let mut empty_cols = vec![];
-    let mut empty_rows = vec![];
-
-    // Check empty columns
-    for x in 0..galaxy.first().unwrap().len() {
-        let is_empty = (0..galaxy.len()).into_iter().map(|y| galaxy.get(y).unwrap().get(x).unwrap()).all(|c| *c == '.');
-        if is_empty {
-           empty_cols.push(x);
-        }
-    }
-
-    // Check empty rows
-    for y in 0..galaxy.len() {
-        let is_empty = (0..galaxy.first().unwrap().len()).into_iter().map(|x| galaxy.get(y).unwrap().get(x).unwrap()).all(|c| *c == '.');
-        if is_empty {
-            empty_rows.push(y);
-        }
-    }
-
-    // Insert 'x' as extra columns
-    for y in 0..galaxy.len() {
-        for (i, col_no) in empty_cols.iter().enumerate() {
-            galaxy.get_mut(y).unwrap().insert(col_no + i, 'x');
-        }
-    }
-
-    // Insert 'y' as extra columns
-    for (i, row_no) in empty_rows.into_iter().enumerate() {
-        let new_row = (0..galaxy.first().unwrap().len()).map(|_| 'y').collect::<Vec<char>>();
-        galaxy.insert(row_no + i, new_row);
-
-    }
-
     galaxy
 }
 
-
-fn calc_dist(x0:usize, y0:usize, x1:usize, y1:usize, dist:usize, galaxy:&Vec<Vec<char>>) -> usize {
-    let x_start = min(x0, x1);
-    let x_end = max(x0, x1);
-    let y_start = min(y0, y1);
-    let y_end = max(y0, y1);
-
-    let x_diff = (x_start..x_end)
-        .map(|x| galaxy.get(y_start).unwrap().get(x).unwrap())
-        .map(|ch| {
-            match *ch {
-                'x' => dist,
-                _ => 1,
-            }
-        }).sum::<usize>();
-
-    let y_diff = (y_start..y_end)
-        .map(|y| galaxy.get(y).unwrap().get(x_start).unwrap())
-        .map(|ch| {
-            match *ch {
-                'y' => dist,
-                _ => 1,
-            }
-        }).sum::<usize>();
-
-    x_diff + y_diff
-}
 
 fn calc_total_dist(lines : Vec<&str>, empty_dist:usize) -> usize {
     let galaxy_map = parse(lines);
@@ -87,14 +26,44 @@ fn calc_total_dist(lines : Vec<&str>, empty_dist:usize) -> usize {
         .flatten()
         .collect();
 
+    let mut empty_cols = vec![];
+    let mut empty_rows = vec![];
+
+    // Check empty columns
+    for x in 0..galaxy_map.first().unwrap().len() {
+        let is_empty = (0..galaxy_map.len()).into_iter().map(|y| galaxy_map.get(y).unwrap().get(x).unwrap()).all(|c| *c == '.');
+        if is_empty {
+            empty_cols.push(x);
+        }
+    }
+
+    // Check empty rows
+    for y in 0..galaxy_map.len() {
+        let is_empty = (0..galaxy_map.first().unwrap().len()).into_iter().map(|x| galaxy_map.get(y).unwrap().get(x).unwrap()).all(|c| *c == '.');
+        if is_empty {
+            empty_rows.push(y);
+        }
+    }
+
     let mut sum = 0;
     for galaxy_no in 0..galaxies.len() {
         let (x0,y0) = galaxies[galaxy_no];
         for other_no in galaxy_no..galaxies.len() {
             if galaxy_no != other_no {
                 let (x1,y1) = galaxies[other_no];
-                // Calc manhattan dist
-                sum += calc_dist(x0, y0, x1, y1, empty_dist - 1, &galaxy_map);
+                // Calc distance and count number of empty passes
+                let x_start = min(x0, x1);
+                let x_end = max(x0, x1);
+                let y_start = min(y0, y1);
+                let y_end = max(y0, y1);
+
+                let number_passes_col = empty_cols.iter().filter(|x| **x >= x_start && **x < x_end).count();
+                let x_diff = x_end - x_start;
+                sum += number_passes_col * empty_dist + (x_diff - number_passes_col);
+
+                let number_passes_row = empty_rows.iter().filter(|y| **y >= y_start && **y < y_end).count();
+                let y_diff = y_end - y_start;
+                sum += number_passes_row * empty_dist + (y_diff - number_passes_row);
             }
         }
     }
